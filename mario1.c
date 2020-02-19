e052cb93ca13c65385254e6be07a2c7c99edadbc
NardinAwad <60411475+NardinAwad@users.noreply.github.com>
2020-02-19 16:21:10 -0500

    Add files via upload

---
+++ mario1.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    
+    do
+    {
+        n = get_int("Height: ");    
+    }
+    while (n < 1 || n > 8);
+    int p =0;
+    int x =n;
+    for (int i = 0; i < n; i++)
+    {
+        p++;
+        x--;
+        
+        
+        for (int j = 0; j<x; j++)
+        {
+            printf(" ");
+        }
+            for (int m = 0; m<p; m++)
+            {
+                printf("#");
+            }
+        
+        printf("\n");
+    }
+}
